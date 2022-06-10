d907a6ea7bf89083e77d032e7e4b4b89cb099e59
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-06-10 10:50:04 +0430

    add modal and its functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Modal from './components/Modal';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 
+import { useSelector } from 'react-redux';
+
 function App() {
+  const modalIsOpen = useSelector(state => state.modal.modalIsOpen);
+
   return (
     <main>
+      {modalIsOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
